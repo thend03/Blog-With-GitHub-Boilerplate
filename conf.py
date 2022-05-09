8288847d5b8cc6d84d69f40da855f696da9905fd
feng.chuang <fengchuang033@zto.com>
2022-05-09 22:04:07 +0800

    add jsdelivr

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "thend03/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
