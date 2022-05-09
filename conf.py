3d0ef4abbb1d84044def4d15c844f43de0722b86
feng.chuang <fengchuang033@zto.com>
2022-05-09 21:35:47 +0800

    test push

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "thend03's blog"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+site_build_date = "2022-05-09T21:33+08:00"
+author = "thend03"
+email = "thend03@163.com"
+author_homepage = "https://www.thend03.com"
+description = "宝宝宝"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
