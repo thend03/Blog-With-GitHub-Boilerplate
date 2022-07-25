2d321d86e520c07bb66af7dfff296367613dca14
feng.chuang <fengchuang033@zto.com>
2022-07-25 15:46:24 +0800

    theme

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Kepler",
+    "name": "icarus",
     "type": "git",
-    "url": "https://github.com/AlanDecode/Maverick-Theme-Kepler.git",
+    "url": "https://github.com/ppoffice/hexo-theme-icarus.git",
     "branch": "latest"
 }
 enable_jsdelivr = {
